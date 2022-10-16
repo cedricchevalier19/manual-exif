14c126c90b73c650eda7d76d2c32735b6ea02f39
Cedric Chevalier <cedric.chevalier@cea.fr>
2022-10-16 19:04:38 +0200

    Read exif in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::path::Path;
+
+use exif;
+
+fn read_exif(path: &Path) {
+    let file = std::fs::File::open(path).unwrap();
+    let mut bufreader = std::io::BufReader::new(&file);
+    let exifreader = exif::Reader::new();
+    let exif = exifreader.read_from_container(&mut bufreader).unwrap();
+    for f in exif.fields() {
+        println!(
+            "{} {} {}",
+            f.tag,
+            f.ifd_num,
+            f.display_value().with_unit(&exif)
+        );
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    read_exif(Path::new("tests/img.jpg"));
 }
